29d8ed3bdc4ad3494c23e3e60dade4de24c20071
Roman Arkhipov <aar.rumm@gmail.com>
2020-06-25 13:27:06 +0200

    Implement HanmarkAuctionModel to AuctionDraft mapping service * implement StateRepo tests

--- src/test/java/com/veragg/website/repository/StateRepo_when_findByZip_is_called.java
+++ src/test/java/com/veragg/website/repository/StateRepo_when_findByZip_is_called.java
@@ -1,13 +1,16 @@
 package com.veragg.website.repository;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.veragg.website.domain.State;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -16,20 +19,43 @@ public class StateRepo_when_findByZip_is_called {
     @Autowired
     StateRepo stateRepo;
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
-    public void given_smth_then_smth_should_happen() {
+    public void given_valid_zip_code_then_state_should_return() {
 
         //Arrange
 
         //Act
-        Object result = stateRepo.findByZip("");
+        State result = stateRepo.findByZip("12439");
 
         //Assert
         assertNotNull(result);
+        assertEquals(result.getId(), "BE");
+
+    }
+
+    @Test
+    public void given_invalid_zip_code_then_null_should_return() {
+
+        //Arrange
+
+        //Act
+        State result = stateRepo.findByZip("999999");
+
+        //Assert
+        assertNull(result);
+
+    }
+
+    @Test
+    public void given_null_zip_code_then_NPE_should_thrown() {
+
+        //Arrange
+
+        //Act
+        State result = stateRepo.findByZip(null);
+
+        //Assert
+        assertNull(result);
 
     }
 
