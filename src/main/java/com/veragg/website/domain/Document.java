801c371f63dda3b1c18bdbd4a99d8d1130d38736
Roman Arkhipov <aar.rumm@gmail.com>
2020-07-27 10:46:07 +0200

    Implement download files * implement DownloadService and its' implementation * implement unit tests, refactor * implement FileManager, custom FileManagementException

--- src/main/java/com/veragg/website/domain/Document.java
+++ src/main/java/com/veragg/website/domain/Document.java
@@ -1,5 +1,7 @@
 package com.veragg.website.domain;
 
+import java.util.UUID;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -8,26 +10,50 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import static java.util.Objects.isNull;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Document {
 
-    //should be base entity without table
+    //TODO: should be base entity without table
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String path;
+    private String url;
 
     @Enumerated(EnumType.STRING)
-    private DocumentType type;
+    private DocumentType documentType;
+
+    @Enumerated(EnumType.STRING)
+    private FileType fileType;
 
     private String originalName;
+    private String storeName;
 
     private Integer sortOrder;
+
+    public Document(String url, DocumentType documentType) {
+
+        //TODO: extract original name
+        this.originalName = String.valueOf(url.charAt(0));
+        //TODO: extract type
+        this.documentType = documentType;
+
+        this.fileType = FileType.valueOf(String.valueOf(url.charAt(0)));
+
+        if (isNull(storeName)) {
+            this.storeName = UUID.randomUUID().toString();
+        }
+
+    }
 
 }
