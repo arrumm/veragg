fccc0720cd360e97abfcc7aafcf703e49f812296
Roman Arkhipov <aar.rumm@gmail.com>
2020-08-26 22:32:26 +0200

    Implement Draft to Auction merge scheduled job * async run of merge job

--- src/main/java/com/veragg/website/jobs/DraftToAuctionMergeJob.java
+++ src/main/java/com/veragg/website/jobs/DraftToAuctionMergeJob.java
@@ -3,12 +3,15 @@ package com.veragg.website.jobs;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.veragg.website.domain.AuctionDraft;
 import com.veragg.website.services.AuctionMergeService;
 
+@EnableAsync
 @Component
 public class DraftToAuctionMergeJob {
 
@@ -19,11 +22,11 @@ public class DraftToAuctionMergeJob {
         this.auctionMergeService = auctionMergeService;
     }
 
+    @Async
     @Scheduled(cron = "0 0 13 * * *")
     public void run() {
 
         List<AuctionDraft> sortedDrafts = auctionMergeService.getSortedDrafts();
-
         sortedDrafts.forEach(auctionDraft -> auctionMergeService.merge(auctionDraft));
 
     }
