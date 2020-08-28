275c9015b8e1a0f57bfc4bd6edc87ff83c55eb9e
Roman Arkhipov <aar.rumm@gmail.com>
2020-08-28 23:02:21 +0200

    Implement download Auction files service and scheduled job * fix async config

--- src/main/java/com/veragg/website/config/AsyncConfig.java
+++ src/main/java/com/veragg/website/config/AsyncConfig.java
@@ -1,18 +1,16 @@
 package com.veragg.website.config;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
-
-    @Bean(name = "threadPoolTaskExecutor")
-    public Executor threadPoolTaskExecutor() {
-        return new ThreadPoolTaskExecutor();
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler();
     }
 }
