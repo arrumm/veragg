4b2331f5b7751b85b4abe3ee49f868ee8e828e8d
Roman Arkhipov <aar.rumm@gmail.com>
2020-09-09 22:32:59 +0200

    Implement Auction controller * add ping method to PageData * refactor to use series of status codes

--- src/main/java/com/veragg/website/crawler/PageData.java
+++ src/main/java/com/veragg/website/crawler/PageData.java
@@ -2,6 +2,7 @@ package com.veragg.website.crawler;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
@@ -11,11 +12,14 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import static java.util.Objects.nonNull;
+import static org.springframework.http.HttpStatus.Series.REDIRECTION;
+import static org.springframework.http.HttpStatus.Series.SUCCESSFUL;
 
 @Getter
 @Setter
@@ -27,6 +31,8 @@ public class PageData {
 
     private String content;
 
+    private Boolean available = Boolean.FALSE;
+
     public PageData(String url) {
         this.url = url;
     }
@@ -35,7 +41,8 @@ public class PageData {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             try (CloseableHttpResponse response = httpClient.execute(new HttpGet(this.url))) {
                 int status = response.getStatusLine().getStatusCode();
-                if (status == 200) {
+                updateAvailability(status);
+                if (Boolean.TRUE.equals(isAvailable())) {
                     HttpEntity entity = response.getEntity();
                     if (nonNull(entity)) {
                         this.content = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
@@ -48,4 +55,21 @@ public class PageData {
         return this;
     }
 
+    public PageData ping() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            try (CloseableHttpResponse response = httpClient.execute(new HttpGet(this.url))) {
+                updateAvailability(response.getStatusLine().getStatusCode());
+            }
+        }
+        return this;
+    }
+
+    private void updateAvailability(int status) {
+        HttpStatus.Series series = HttpStatus.Series.resolve(status);
+        this.available = Arrays.asList(SUCCESSFUL, REDIRECTION).contains(series) ? Boolean.TRUE : this.available;
+    }
+
+    public Boolean isAvailable() {
+        return available;
+    }
 }
